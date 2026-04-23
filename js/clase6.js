cc6113958e3b0a004c8f96abfa0bfe1f167b825c
Emanuel Ramos <emanuelramos19@outlook.com>
2026-04-22 21:00:22 -0300

    Clase 6: Medidas relativas, quienes somos e introduccion a JS

---
+++ js/clase6.js
@@ -0,0 +1,10 @@
+/* document.getElementById("nombre").textContent = "Emanuel";
+document.getElementById("ciudad").textContent = "de la fueria";
+ */
+
+const nombre = "Emanuel";
+const apellido = "Ramos";
+const ciudad = "de la furia";
+
+document.getElementById("nombre").textContent = nombre + " " + apellido;
+document.getElementById("ciudad").textContent = ciudad;
