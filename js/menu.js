dd4c3fd5617f78f5b832f2494c10046b1ecc18f6
Emanuel Ramos <emanuelramos19@outlook.com>
2026-05-20 14:54:47 -0300

    Clase 9: Simplificacion de js

--- js/menu.js
+++ js/menu.js
@@ -1,55 +1,47 @@
 const primaryNav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".menu-btn");
+const menuBtn = document.querySelector(".menu-btn");
 const cartNav = document.querySelector(".cart-navigation");
-const cartToggle = document.querySelector(".carrito-btn");
+const cartBtn = document.querySelector(".carrito-btn");
 const closeAll = document.querySelector(".fantasma");
 
 /* funcion para resetear y evitar errores en los btn */
 /* quita la clase auxiliar que los oculta cuando el otro esta activo */
 const resetButtons = () => {
-    navToggle.classList.remove("ocultar");
-    cartToggle.classList.remove("ocultar");
+    menuBtn.classList.remove("ocultar");
+    cartBtn.classList.remove("ocultar");
 };
 
-navToggle.addEventListener("click", () => {
+/* Funcion que muestra el panel segun el btn que se activa */
+const mostrarPanel = (elementoNav, activeBtn, ocultaBtn) => {
     const isVisible = primaryNav.getAttribute("data-visible") === "true";
 
-    primaryNav.setAttribute("data-visible", !isVisible);
-    navToggle.setAttribute("aria-expanded", !isVisible);
+    elementoNav.setAttribute("data-visible", !isVisible);
+    activeBtn.setAttribute("aria-expanded", !isVisible);
 
     if (!isVisible) {
-        cartToggle.classList.add("ocultar");
+        ocultaBtn.classList.add("ocultar");
         closeAll.classList.add("activo"); /* activo el "fantasma" */
     } else {
         resetButtons();
         closeAll.classList.remove("activo");
     }
-});
+};
 
-cartToggle.addEventListener("click", () => {
-    const isVisible = cartNav.getAttribute("data-visible") === "true";
-
-    cartNav.setAttribute("data-visible", !isVisible);
-    cartToggle.setAttribute("aria-expanded", !isVisible);
-
-    if (!isVisible) {
-        navToggle.classList.add("ocultar");
-        closeAll.classList.add("activo");
-    } else {
-        resetButtons();
-        closeAll.classList.remove("activo");
-    }
-});
-
-closeAll.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute("data-visible");
-
+const cerrarTodo = () => {
     primaryNav.setAttribute("data-visible", false);
-    navToggle.setAttribute("aria-expanded", false);
+    menuBtn.setAttribute("aria-expanded", false);
 
     cartNav.setAttribute("data-visible", false);
-    cartToggle.setAttribute("aria-expanded", false);
+    cartBtn.setAttribute("aria-expanded", false);
 
     closeAll.classList.remove("activo");
     resetButtons();
-});
+};
+
+menuBtn.addEventListener("click", () =>
+    mostrarPanel(primaryNav, menuBtn, cartBtn),
+);
+cartBtn.addEventListener("click", () =>
+    mostrarPanel(cartNav, cartBtn, menuBtn),
+);
+closeAll.addEventListener("click", cerrarTodo);
